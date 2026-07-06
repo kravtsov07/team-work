4c895e8c1983422858034e58131b6f19846f9419
StepanKravcov <kravtsov.sa07@mail.ru>
2026-07-06 19:10:29 +0300

    full(0.(9))

--- src/back/GA.py
+++ src/back/GA.py
@@ -7,6 +7,26 @@ n - 2 операций
 
 """
 
+def calculate_min_cost(dimensions: list[int]) -> int:
+    n = len(dimensions) - 1
+    if n <= 0:
+        return 0
+        
+    dp = [[0] * n for _ in range(n)]
+    
+    for l in range(2, n + 1):
+        for i in range(n - l + 1):
+            j = i + l - 1
+            dp[i][j] = float('inf')
+            
+            for k in range(i, j):
+                cost = dp[i][k] + dp[k + 1][j] + dimensions[i] * dimensions[k + 1] * dimensions[j + 1]
+                
+                if cost < dp[i][j]:
+                    dp[i][j] = cost
+                    
+    return dp[0][n - 1]
+
 def generate_dimensions(dim_size: int = 100, min_size: int = 1, max_size: int = 10):
     return [rd.randint(min_size, max_size) for _ in range(dim_size)]
 
@@ -14,13 +34,13 @@ def generate_population(pop_size: int, ind_size: int) -> list[list[int]]:
     return [generate_individual(ind_size) for _ in range(pop_size)]
 
 def generate_individual(ind_size: int):
-    return [rd.randint(0, ind_size - 2 - i) for i in range(ind_size - 2)]
+    return [rd.randint(0, ind_size - 3 - i) for i in range(ind_size - 2)]
 
 def is_valid_individual(individual: list[int], n: int) -> bool:
     if len(individual) != n - 2:
         return False
         
-    return all(0 <= val <= (n - 2 - i) for i, val in enumerate(individual))
+    return all(0 <= val <= (n - 3 - i) for i, val in enumerate(individual))
 
 def calculate_cost(dimensions: list[int], individual: list[int]):
     cost = 0
@@ -42,7 +62,7 @@ def mutate(individual: list[int], p_m: float = None):
         
     for i in range(ind_size):
         if rd.random() < p_m:
-            individual[i] = rd.randint(0, ind_size - i)
+            individual[i] = rd.randint(0, ind_size - 1 - i)
             
     return individual
 
@@ -55,7 +75,7 @@ def crossover(first_parent: list[int], second_parent: list[int]):
         second_child
     )
 
-def selection(population: list[list[int]], dim: list[int], tournament_size: int = 3, p_c: float = 0.8):
+def selection(population: list[list[int]], dim: list[int], tournament_size: int = 3, p_c: float = 0.8) -> list[list[int]]:
     next_population = []
     len_next_population = 0
     
@@ -78,11 +98,36 @@ def selection(population: list[list[int]], dim: list[int], tournament_size: int 
         
     return next_population
 
-n = 5
-
-dim = generate_dimensions(n)
-
-ind = generate_individual(n)
-print(dim)
-print(ind)
-print(calculate_cost(dim, ind))
+def genetic_algorithm(population_size: int,
+                      max_generations: int,
+                      dim_size: int,
+                      dimensions: list[int] = None,
+                      ):
+    
+    if not dimensions:
+        dimensions = generate_dimensions(dim_size)
+    
+    best_cost = float('inf')
+    min_cost = calculate_min_cost(dimensions)
+    print(min_cost)
+    generations = 0
+    population = generate_population(population_size, dim_size)
+    while generations < max_generations:
+        population = selection(population, dimensions, )
+        best_ind = min(population, key=lambda ind: calculate_cost(dimensions, ind))
+        best_cost = calculate_cost(dimensions, best_ind)
+        if best_cost == min_cost:
+            print(f"Найдено лучшее решение: {best_ind}, стоимость: {best_cost}")
+            break
+        all_cost = 0
+        for ind in population:
+            all_cost += calculate_cost(dimensions, ind)
+        
+        mean_cost = all_cost / population_size
+        print(f"Поколение {generations + 1}: Лучший - {best_cost}, Средний - {mean_cost}")
+        generations += 1
+        
+    
+if __name__ == "__main__":
+    dim_test4 = generate_dimensions(dim_size=45, min_size=10, max_size=150)
+    genetic_algorithm(population_size=500, max_generations=400, dim_size=45, dimensions=dim_test4)
