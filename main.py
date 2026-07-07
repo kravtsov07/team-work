4782969f2bdc8f04fd0d904ab832ce9e07ee5363
Babii Dmitrii <zakprfilenina@gmail.com>
2026-07-07 03:59:44 +0300

    stub for main

---
+++ main.py
@@ -0,0 +1,15 @@
+# from src.gui.... import Window
+from PySide6.QtWidgets import QApplication
+
+
+def main():
+    app = QApplication([])
+
+    # window = MainWindow()
+    # window.show()
+
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
